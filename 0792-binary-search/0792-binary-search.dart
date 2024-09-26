e0187765a69992ad65fe3add8393c52dda7524ff
Salman Faris K <142564106+salmanfaris22@users.noreply.github.com>
2024-09-26 15:51:17 +0530

    Time: 371 ms (14.56%), Space: 156.7 MB (43.69%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.dart
@@ -0,0 +1,12 @@
+class Solution {
+   search(List<int> nums, int target) {
+    
+    for(int i=0;i<nums.length;i++){
+        if(target==nums[i]){
+            return i;
+        }
+    }
+    return -1;
+  }
+ 
+}
