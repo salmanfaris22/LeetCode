645b6279e822153afc2b21bc6c7ab5c8a08054bb
Salman Faris K <142564106+salmanfaris22@users.noreply.github.com>
2024-10-06 14:11:00 +0530

    Time: 4 ms (80.3%), Space: 5 MB (69.79%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.go
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.go
@@ -6,15 +6,16 @@
  * }
  */
 func removeElements(head *ListNode, val int) *ListNode {
-    dummy := new(ListNode)
-	dummy.Next = head
-	p := dummy
-	for p.Next != nil {
-		if p.Next.Val == val {
-			p.Next = p.Next.Next
-		} else {
-			p = p.Next
-		}
-	}
-	return dummy.Next
+    dummy:=&ListNode{Next:head}
+    temp := dummy
+    for temp.Next != nil {
+       
+        
+        if temp.Next.Val == val {
+            temp.Next = temp.Next.Next
+        }else{
+         temp = temp.Next
+        }
+    } 
+    return dummy.Next
 }
