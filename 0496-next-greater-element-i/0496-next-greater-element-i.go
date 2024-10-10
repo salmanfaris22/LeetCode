d1010c084a2a0c622f83085876cade87d8ebe19f
Salman Faris K <142564106+salmanfaris22@users.noreply.github.com>
2024-10-10 09:58:58 +0530

    Time: 5 ms (12.74%), Space: 2.9 MB (86.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.go
+++ 0496-next-greater-element-i/0496-next-greater-element-i.go
@@ -1,25 +1,19 @@
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
-       arr:=[]int{}
+ arr:=[]int{}
     for i:=0;i<len(nums1);i++{
-        f:=0
-         p:=0
+        f,p:=0,0
         for j:=0;j<len(nums2);j++{
-           
             if nums1[i] == nums2[j] || f>=1{
                 f++
                  if nums1[i]<nums2[j]{
                      arr = append(arr,nums2[j])
                      p++
                      break
-                     
                  }
             }
-            
-            
         }
         if p==0{
-                arr=append(arr,-1)
-                
+                arr=append(arr,-1)   
             }
     }
     
