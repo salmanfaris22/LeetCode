aa1d687ad1a7615ee98e4e8ccc2e6944c1ca121a
Salman Faris K <142564106+salmanfaris22@users.noreply.github.com>
2024-10-16 09:38:51 +0530

    Time: 1 ms (75.17%), Space: 2.2 MB (96.22%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.go
@@ -0,0 +1,15 @@
+func plusOne(digits []int) []int {
+     n := len(digits)
+    
+
+    for i := n - 1; i >= 0; i-- {
+        digits[i]++ 
+        if digits[i] < 10 { 
+            return digits
+        }
+        digits[i] = 0  
+    }
+    
+
+    return append([]int{1}, digits...)
+}
