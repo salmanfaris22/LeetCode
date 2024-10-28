4bcc6a4bf46cb10700d5c274b5ecb118d5ef187e
Salman Faris K <142564106+salmanfaris22@users.noreply.github.com>
2024-10-28 19:06:25 +0530

    Time: 10 ms (34.23%), Space: 59 MB (49.25%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n==1){
+        return true
+    }
+    f=n
+    l=0
+     for( i=1;i<f;i+=0){
+        f=f/3
+        l++
+     }
+     return Math.pow(3,l)==n
+
+}
