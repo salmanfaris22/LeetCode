556ded05205ad4e17823cee3ae648917d621b33b
Salman Faris K <142564106+salmanfaris22@users.noreply.github.com>
2024-10-28 18:48:54 +0530

    Time: 99 ms (9.52%), Space: 54.9 MB (56.3%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.js
+++ 2886-faulty-keyboard/2886-faulty-keyboard.js
@@ -3,15 +3,13 @@
  * @return {string}
  */
 var finalString = function(s) {
- s=s.split("")
-    c=""
-    for(i=0;i<s.length;i++){
-        if(s[i]=="i"){
-          s=(c.split("").reverse().join("")+s.splice(i+1).join("")).split("")
-          i=0
-          c=""
+ v=[]
+   for(let i of s){
+        if(i=="i"){
+            v.reverse()
+        }else{
+            v.push(i)
         }
-        c+=s[i]
-    }
-    return  s.join("")
+   }
+   return v.join("")
 };
